e781af01ce5a6373cb83483e5e0f137a6bf680e7
ThatGuyFromFinland <ahti.ahde@gmail.com>
2016-02-06 00:08:38 +0000

    WIP, need to fix POST parameters

--- lib/GlomeClient.php
+++ lib/GlomeClient.php
@@ -30,10 +30,61 @@ class GlomeClient
         if ($debugMode) {
             try {
                 // Guzzle should throw exceptions by default for non 2xx responses
-                $this->httpClient->get('applications/check/' . base64_encode($this->apiUserId) . '.json');
+                $this->get('applications/check/' . base64_encode($this->apiUserId) . '.json');
             } catch (ClientException $ex) {
                 throw new GlomeException('Invalid Api Credentials');
             }
         }
+    }
+
+    private function get($path)
+    {
+        $body = $this->httpClient->get($path)->getBody();
+        return json_decode($body->read($body->getSize()), true);
+    }
+
+    private function post($path, $params = [])
+    {
+        $options = [];
+        if (is_array($params) && count($params) > 0) {
+            $options['query'] = $this->httpClient->getConfig()['query'] + $params;
+        }
+
+        $body = $this->httpClient->post($path, $options)->getBody();
+        return json_decode($body->read($body->getSize()), true);
+    }
+
+    public function createUser()
+    {
+        return $this->post('users.json');
+    }
+
+    public function getUserProfile($glomeId)
+    {
+        return $this->get('users/' . $glomeId . '.json');
+    }
+
+    public function getSyncCode($glomeId, $kind = 'b')
+    {
+        return $this->post('users/' . $glomeId . '/sync.json',
+            ['synchronization[kind]' => $kind]);
+    }
+
+    public function pairSyncCode($glomeId, $syncCode)
+    {
+        $syncSplits = str_split($syncCode, 4);
+
+        return $this->post('users/' . $glomeId . '/sync/pair.json',
+            [
+                'pairing[code1]' => $syncSplits[0],
+                'pairing[code2]' => $syncSplits[1],
+                'pairing[code3]' => $syncSplits[2]
+            ]);
+    }
+
+    public function pairGlomeIds($firstGlomeId, $secondGlomeId)
+    {
+        $syncCode = $this->getSyncCode($firstGlomeId)['code'];
+        return $this->pairSyncCode($secondGlomeId, $syncCode);
     }
 }
--- public/rest/rest.php
+++ public/rest/rest.php
@@ -7,3 +7,13 @@ use ChainRevolution\Demo\GlomeClient;
 
 $configs = json_decode(file_get_contents('config/config.json'), true);
 $glomeClient = new GlomeClient($configs, true);
+try {
+    $firstGlomeId = $glomeClient->createUser()['glomeid'];
+    $secondGlomeId = $glomeClient->createUser()['glomeid'];
+    $glomeClient->pairGlomeIds($firstGlomeId, $secondGlomeId);
+    var_dump($glomeClient->getUserProfile($firstGlomeId));
+    var_dump($glomeClient->getUserProfile($secondGlomeId));
+}
+catch (\Exception $ex) {
+    $ex->getMessage();
+}
